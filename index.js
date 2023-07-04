7a56984792f9c1603b4f76e7752e31ea0fc93b18
Tess <tess.merrill25@gmail.com>
2023-07-04 17:28:11 -0600

    complete loop lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const recipients = ["Paige", "Liam", "Trevor"];
+const occassion = "birthday"
+
+function writeCards(recipients, occassion) {
+    let cardArray = [];
+    for (let i = 0; i < recipients.length; i++){
+        cardArray[i] = `Thank you, ${recipients[i]}, for the wonderful ${occassion} gift!`;
+    }
+    return cardArray;
+};
+//writeCards(["Paige", "Liam", "Trevor"], "birthday");
+//instead of logging messages, add into new array
+//then return new array
+
+function countDown(num) {
+    while (num > -1) {
+        console.log(num--);
+    }
+}
